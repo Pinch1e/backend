26c6324f3d078707cb0c81e48fcc7cb5fb009a6b
Pinch1e <arihilamimmanuel@gmail.com>
2025-09-26 18:16:25 +0200

    Just some Update

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,12 +5,13 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS || "https://your-storefront-domain.com,http://localhost:3000",
-      adminCors: process.env.ADMIN_CORS || "https://admin-self-gamma.vercel.app,http://localhost:7001",
-      authCors: process.env.AUTH_CORS || "https://admin-self-gamma.vercel.app,http://localhost:7001",
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET,
+      cookieSecret: process.env.COOKIE_SECRET,
+    },
+  },
 })
