20f47551ff26932ed49fd6abd2d0f10f2c81f9bf
Pinch1e <arihilamimmanuel@gmail.com>
2025-09-26 14:41:13 +0200

    Update

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,9 +6,9 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "https://your-storefront-domain.com,http://localhost:3000",
+      adminCors: process.env.ADMIN_CORS || "https://admin-self-gamma.vercel.app,http://localhost:7001",
+      authCors: process.env.AUTH_CORS || "https://admin-self-gamma.vercel.app,http://localhost:7001",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
