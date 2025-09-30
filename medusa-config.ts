f1d515c2194234108636d59b706331f9fce7fb2e
Pinch1e <arihilamimmanuel@gmail.com>
2025-09-30 17:45:04 +0200

    Wahala

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,17 +1,22 @@
+require("dotenv").config(); // load .env
+
 const config = {
   projectConfig: {
     database_url: process.env.DATABASE_URL,
     redis_url: process.env.REDIS_URL,
-    jwtSecret: process.env.JWT_SECRET,
-    cookieSecret: process.env.COOKIE_SECRET,
-    storeCors: process.env.STORE_CORS,
-    adminCors: process.env.ADMIN_CORS,
-    authCors: process.env.AUTH_CORS,
+    jwt_secret: process.env.JWT_SECRET,
+    cookie_secret: process.env.COOKIE_SECRET,
+    store_cors: process.env.STORE_CORS,
+    admin_cors: process.env.ADMIN_CORS,
+    auth_cors: process.env.AUTH_CORS,
+    database_driver_options: {
+      ssl: { rejectUnauthorized: false },
+    },
     serve: {
-      admin: false,
+      admin: false, // or true if you want the admin server to run
     },
   },
   plugins: [],
-}
+};
 
-export default config
+module.exports = config;
